737a23e08ceb83ce3cdf97e0601b4c3d84a8a15f
Jéssica Carneiro <jessicacarneiro@dcc.ufmg.br>
2016-04-14 14:04:29 -0400

    Basit's classes.

--- src/Bridge_media_package/CheckedOutMedia.java
+++ src/Bridge_media_package/CheckedOutMedia.java
@@ -1,25 +1,109 @@
 package Bridge_media_package;
 
 /**
- * CheckedOutMedia is the class that implements a media that was borrowed
- * by a specific user. It will store state information relevant to this
- * operation. 
- * 
- * @author <a href="jessicacarneiro@ufmg.br">Jéssica Carneiro</a>
+ * CheckedOutMedia is the class that implements a media that was borrowed by a
+ * specific user. It will store state information relevant to this operation.
+ *
+ * @author <a href="abasit1@oldwestbury.edu">Abdul Basit</a>
  * @version 1.0.0
  */
 public class CheckedOutMedia extends Media {
 
+    private String patron_id;
+    private String media_id;
+    private String borrow_date;
+    private String due_date;
+    private String date_returned;
+
     /**
      * Constructor for CheckedOutMedia.
-     * 
+     *
      * @param id Media unique identifier
      * @param title Media title
      * @param year Media publishing year
      * @param cost Media cost
+     * @param patron_id Patrons id
+     * @param media_id Medias id
+     * @param borrow_date date borrowed
+     * @param due_date date due
+     * @param date_returned date returned
      */
-    public CheckedOutMedia(String id, String title, String year, String cost) {
+    public CheckedOutMedia(String id, String title, String year, String cost,
+            String patron_id, String media_id, String borrow_date,
+            String due_date, String date_returned) {
         super(id, title, year, cost);
+    }
+
+    /**
+     * Returns the Patron ID of the Patron who is checking out the media.
+     *
+     * @return patron id
+     */
+    public String getPatron_id() {
+        return patron_id;
+    }
+
+    /**
+     * Returns the Media ID of the Media that is being checked out.
+     *
+     * @return media id
+     */
+    public String getMedia_id() {
+        return media_id;
+    }
+
+    /**
+     * Returns the date that the Media is being checked out.
+     *
+     * @return borrow date
+     */
+    public String getBorrow_date() {
+        return borrow_date;
+    }
+
+    /**
+     * Sets the date that the Media is being checked out.
+     *
+     * @param borrow_date date borrowed
+     */
+    public void setBorrow_date(String borrow_date) {
+
+    }
+
+    /**
+     * Returns the date that the Media is due back.
+     *
+     * @return due date
+     */
+    public String getDue_date() {
+        return due_date;
+    }
+
+    /**
+     * Sets the date that the Media is due back.
+     *
+     * @param due_date date due
+     */
+    public void setDue_date(String due_date) {
+
+    }
+
+    /**
+     * Returns the date that the Media is actually returned.
+     *
+     * @return return date
+     */
+    public String getDate_returned() {
+        return date_returned;
+    }
+
+    /**
+     * Sets the date that the Media is actually returned.
+     *
+     * @param date_returned date returned
+     */
+    public void setDate_returned(String date_returned) {
+
     }
 
 }
--- src/Bridge_media_package/MediaCD.java
+++ src/Bridge_media_package/MediaCD.java
@@ -1,22 +1,28 @@
 package Bridge_media_package;
 
+import java.util.ArrayList;
 
 /**
- * MediaCD is the class for all media material of type CDs.
- * It inherits from Media superclass.
+ * MediaCD is the class for all media material of type CDs. It inherits from
+ * Media superclass.
  * <p>
- * The MediaCD class encapsulates all state information related to CDs.
- * These state information includes: </p>
+ * The MediaCD class encapsulates all state information related to CDs. These
+ * state information includes: </p>
  * <ul>
  * <li></li>
  * <li></li>
  * <li></li>
  * </ul>
  *
- * @author <a href="jessicacarneiro@ufmg.br">Jéssica Carneiro</a>
+ * @author <a href="abasit1@oldwestbury.edu">Abdul Basit</a>
  * @version 1.0.0
  */
 public class MediaCD extends Media {
+
+    private ArrayList<String> artists;
+    private String genre;
+    private String album;
+    private String total_duration;
 
     /**
      * Loan period is the duration of a loan in days.
@@ -47,9 +53,87 @@ public class MediaCD extends Media {
      * @param title Media title
      * @param year Media publishing year
      * @param cost Media cost
+     * @param artists an ArrayList of strings with the name of all artists
+     * @param genre genre of the cd
+     * @param album cd's album name
+     * @param total_duration length of playback
      */
-    public MediaCD(String id, String title, String year,String cost) {
+    public MediaCD(String id, String title, String year, String cost,
+            ArrayList<String> artists, String genre, String album,
+            String total_duration) {
         super(id, title, year, cost);
+    }
+
+    /**
+     * Returns all of the artists for the cd.
+     *
+     * @return an ArrayList of strings with the name of all artists
+     */
+    public ArrayList<String> getArtists() {
+        return artists;
+    }
+
+    /**
+     * Sets the lists of artists for the cd.
+     *
+     * @param artists an ArrayList of strings with the name of all artists
+     */
+    public void setArtists(ArrayList<String> artists) {
+
+    }
+
+    /**
+     * Returns the genre of the cd
+     *
+     * @return genre of cd
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * Sets the genre of the cd.
+     *
+     * @param genre genre of the cd
+     */
+    public void setGenre(String genre) {
+
+    }
+
+    /**
+     * Returns the cd's album name
+     *
+     * @return album of cd
+     */
+    public String getAlbum() {
+        return album;
+    }
+
+    /**
+     * Sets the cd's album name
+     *
+     * @param album cd's album name
+     */
+    public void setAlbum(String album) {
+
+    }
+
+    /**
+     * Returns the length of duration
+     *
+     * @return length of duration
+     */
+    public String getTotal_duration() {
+        return total_duration;
+    }
+
+    /**
+     * Sets the length of the duration
+     *
+     * @param total_duration length of playback
+     */
+    public void setTotal_duration(String total_duration) {
+
     }
 
 }
