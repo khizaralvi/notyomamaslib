e4f80a5b7347d56273186a592e596c788dbb7d9a
Jéssica Carneiro <jessicacarneiro@dcc.ufmg.br>
2016-05-10 19:57:37 -0400

    Changing structure of test folder.

--- test/account/AccountJdbcTest.java
+++ test/account/AccountJdbcTest.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package media.income;
+package account;
 
 /**
  *
  * @author jessicacarneiro
  */
-public class IncomeJdbcTest {
+public class AccountJdbcTest {
     
 }
