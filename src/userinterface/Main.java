b36fec78a03f9ebc6af613584e3096dcf297bfcf
xnible <xNible@gmail.com>
2016-05-17 18:53:17 -0400

    Added a insert method to librarian options working

--- src/userinterface/Main.java
+++ src/userinterface/Main.java
@@ -12,7 +12,7 @@ import java.util.Date;
 
 public class Main {
 
-    public static final String staffMenu = "\n\n\n=========MENU OPTIONS:=========\n1. Look up Account\n2. Media Managment\n3. Update Account\n4. View Income \n5.Insert Income\n0. Logout";
+    public static final String staffMenu = "\n\n\n=========MENU OPTIONS:=========\n1. Look up Account\n2. Media Managment\n3. Update Account\n4. View Income \n5. Insert Income\n0. Logout";
     public static final String patronMenu = "";
     
     
